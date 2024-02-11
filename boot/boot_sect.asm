288ad6eeec443f686284815fb94e9911c0c99a54
Fabian <mail@fabianbissmann.de>
2024-02-11 09:33:33 +0100

    improve boot_sect add todos in print_string_pm

--- boot/boot_sect.asm
+++ boot/boot_sect.asm
@@ -1,19 +1,26 @@
+; TODO: draw memory map
+; TODO: create BPB
+; TODO: boot_drive issue, save dl with pusha
+; DONE.
 [org 0x7c00]
-KERNEL_OFFSET equ 0x1000
+[bits 16]
+
+start:
+JMP 0x0000:flushcs
+
+flushcs:
+xor ax, ax
+mov ds, ax
+mov es, ax
+mov ss, ax
+mov sp, 0x7c00 
 
 mov [BOOT_DRIVE], dl
-
-mov bp, 0x9000
-mov sp, bp
 
 mov bx, MSG_REAL_MODE
 call print_string
 
 call load_kernel
-
-call switch_to_pm
-
-jmp $
 
 %include "tools/print_string.asm"
 %include "tools/disk_load.asm"
@@ -30,7 +37,7 @@ load_kernel:
   mov dh, 15
   mov dl, [BOOT_DRIVE]
   call disk_load
-
+  call switch_to_pm
   ret
 
 [bits 32]
@@ -39,13 +46,18 @@ BEGIN_PM:
   call print_string_pm
 
   call KERNEL_OFFSET
-
+  mov ebx, MSG_RET_KERNEL
+  call print_string_pm
   jmp $
 
 BOOT_DRIVE db 0
 MSG_REAL_MODE db "Started in 16-bit real mode", 0
 MSG_PROT_MODE db "Successfully landed in 32-bit protected mode", 0
 MSG_LOAD_KERNEL db "Loading kernel into memory.", 0
+MSG_RET_KERNEL db "Returned from kernel.", 0
 
 times 510-($-$$) db 0
 dw 0xaa55
+
+KERNEL_OFFSET:
+incbin "kernel/kernel.bin"
--- tools/print_string_pm.asm
+++ tools/print_string_pm.asm
@@ -1,21 +1,32 @@
+; TODO understand below
+; lodsb
+; L:
+; stosw
+; lodsb
+; test al, al
+; jnz L
+; TODO: change parameter from ebx to esi to use lodsb
+
 [bits 32]
 VIDEO_MEMORY equ 0xb8000
 WHITE_ON_BLACK equ 0x0f
 
 print_string_pm:
-	pusha
-	mov edx, VIDEO_MEMORY
-
-print_string_pm_loop:
-	mov al, [ebx]
+	pusha ; TODO: check use; use di and define a contract with the caller that di must be used
+	mov edx, VIDEO_MEMORY ; di for using stosb
 	mov ah, WHITE_ON_BLACK
 
-	cmp al, 0
-	je print_string_pm_done
+; lodsb (to load es:edi to al)
+print_string_pm_loop:
+	; stosw (store ax at address es:edi), if edi is &video_memory
+	mov al, [ebx] ; lodsb (
 
-	mov [edx], ax
+	test al, al
+	je print_string_pm_done ; jnz string_pm_loop
 
-	add ebx, 1
+	mov [edx], ax ; store character in video memory
+
+	inc ebx
 	add edx, 2
 
 	jmp print_string_pm_loop
@@ -23,3 +34,4 @@ print_string_pm_loop:
 print_string_pm_done:
 	popa
 	ret
+
